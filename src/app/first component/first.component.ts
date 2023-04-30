32366685a2ff830b459a3fda0aeb22000472b4b9
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-30 13:52:30 +0300

    created the first ng component ever

---
+++ src/app/first component/first.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom-heading',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css'],
+})
+export class FirstComponent {
+  // Here is the logic
+}
