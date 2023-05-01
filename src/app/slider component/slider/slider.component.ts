32b69c14c80eb2570ab0ce52d2e474745fcb07d4
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-01 21:19:52 +0300

    implemented the next and previous functionalities

--- src/app/slider component/slider/slider.component.ts
+++ src/app/slider component/slider/slider.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.css'],
 })
 export class SliderComponent {
+  images = [
+    '0.jpg',
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg',
+    '10.jpg',
+  ];
+  src = '0';
 
+  next() {
+    this.src =
+      parseInt(this.src) == this.images.length - 1
+        ? `${this.images.length - 1}`
+        : `${parseInt(this.src) + 1}`;
+  }
+
+  prev() {
+    this.src = parseInt(this.src) == 0 ? '0' : `${parseInt(this.src) - 1}`;
+  }
 }
