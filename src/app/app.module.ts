33019fc094b19f3f4a16e160a3069b21614c7d35
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-01 17:33:33 +0300

    used ngModel to implement a two-way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first component/first.component';
 import { InputBindingComponent } from './second component/input-binding/input-binding.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, FirstComponent, InputBindingComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
