361248561e1829bf65baf1ab87dce43a1f7097fd
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-01 17:56:56 +0300

    added the slider component and organized the file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app component/app.component';
 import { FirstComponent } from './first component/first.component';
 import { InputBindingComponent } from './second component/input-binding/input-binding.component';
+import { SliderComponent } from './slider component/slider/slider.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, InputBindingComponent],
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    InputBindingComponent,
+    SliderComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
