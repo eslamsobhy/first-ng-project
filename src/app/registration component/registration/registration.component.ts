0ee5aaac76694c19f7812e51c5eab0c8f0378b84
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-02 21:00:37 +0300

    reviewed passing data from parent to child

--- src/app/registration component/registration/registration.component.ts
+++ src/app/registration component/registration/registration.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent {
+  // studentObj: object = {};
+  // myEvent = new EventEmitter();
+  // name = '';
+  // age = '';
 
+  // add() {}
+
+  @Input() name: string = ''; //receiving the data from parent
 }
