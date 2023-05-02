96a7e602d8569828d0503e95fbcca763eef69432
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-02 21:44:45 +0300

    reviewed passing data from child to parent

--- src/app/app component/app.component.ts
+++ src/app/app component/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'first-ng-project';
   parentName = 'App';
+
+  dataFromChild = '';
+
+  getData(data: any) {
+    this.dataFromChild = data;
+  }
 }
--- src/app/students component/students/students.component.ts
+++ src/app/students component/students/students.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+  styleUrls: ['./students.component.css'],
 })
 export class StudentsComponent {
+  data = 'data from child!';
 
+  // 1)define the event
+  @Output() getEvent = new EventEmitter();
+
+  // 2) when will the event fire?
+  constructor() {
+    setTimeout(() => {
+      this.getEvent.emit(this.data);
+    }, 2000);
+  }
 }
